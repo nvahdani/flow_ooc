0e96a9950165fafd14842cb705cb94badca19697
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-24 13:56:35 +0200

    05_samtobam110trimmed.sh

--- 05_samtobam110trimmed.sh
+++ 05_samtobam110trimmed.sh
@@ -4,14 +4,14 @@
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --time=01:00:00
 #SBATCH --job-name=samtobam
-#SBATCH --output=//data/users/nvahdani/flow_project/samtobam_110/output_samtobam110trimmed%j.o
-#SBATCH --error=/data/users/nvahdani/flow_project/samtobam_110/error_samtobam110trimmed%j.e
+#SBATCH --output=/samtobam_110/output_samtobam110trimmed%j.o
+#SBATCH --error=/samtobam_110/error_samtobam110trimmed%j.e
 
-OUTDIR="/data/users/nvahdani/flow_project/samtobam_110/output_trimmed"
-SAMDIR="/data/users/nvahdani/flow_project/mapping110/output_trimmed"
+OUTDIR="/samtobam_110/output_trimmed"
+SAMDIR="/mapping110/output_trimmed"
 
 
-R1=($(ls -1 /data/users/nvahdani/flow_project/reads/trimmed_data_newversion/*R1*.trimmed.fastq.gz))
+R1=($(ls -1 /reads/trimmed_data_newversion/*R1*.trimmed.fastq.gz))
 filename=$(basename "${R1[$SLURM_ARRAY_TASK_ID]}"| sed 's/_L.*//')
 base=${filename%.*}
 echo $base
