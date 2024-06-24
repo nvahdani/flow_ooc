0d0f130b94c5851b7db6c298dddd60dbca4eb73f
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-24 13:51:23 +0200

    01_fastqc_newtrim.sh

--- 01_fastqc_newtrim.sh
+++ 01_fastqc_newtrim.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=03:00:00
 #SBATCH --job-name=fastqctrim
-#SBATCH --output=/data/users/nvahdani/flow_project/fastqc_newtrim/output_fastqc_newtrim_%j.o
-#SBATCH --error=/data/users/nvahdani/flow_project/fastqc_newtrim/error_fastqc_newtrim_%j.e
+#SBATCH --output=/fastqc_newtrim/output_fastqc_newtrim_%j.o
+#SBATCH --error=/fastqc_newtrim/error_fastqc_newtrim_%j.e
 
 module add UHTS/Quality_control/fastqc/0.11.9;
 fastqc -o /data/users/nvahdani/flow_project/fastqc_newtrim/output -f fastq /data/users/nvahdani/flow_project/reads/trimmed_data_newversion/*.fastq.gz
