4fffe51c7488ccfbcf829904681131444456ee6f
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-24 13:37:58 +0200

    0_uploaddata.sh

--- 0_uploaddata.sh
+++ 0_uploaddata.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=1000MB
 #SBATCH --time=01:00:00
 #SBATCH --job-name=data_upload
-#SBATCH --output=/data/users/nvahdani/flow_project/reads/data_secondupload/output_secondupload_%j.o
-#SBATCH --error=/data/users/nvahdani/flow_project/reads/data_secondupload/error_secondupload_%j.e
+#SBATCH --output=/reads/data_secondupload/output_secondupload_%j.o
+#SBATCH --error=/reads/data_secondupload/error_secondupload_%j.e
 
 wget -i /data/users/nvahdani/flow_project/reads/urls.txt -P /data/users/nvahdani/flow_project/reads/data_secondupload 
