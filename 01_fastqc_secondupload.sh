c31139336a9c5d4150416186b438772d1576e30f
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-24 13:50:17 +0200

    01_fastqc_secondupload.sh

--- 01_fastqc_secondupload.sh
+++ 01_fastqc_secondupload.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=03:00:00
 #SBATCH --job-name=fastqc_secondupload
-#SBATCH --output=/data/users/nvahdani/flow_project/fastqc_secondupload/output_fastqc_secondupload_%j.o
-#SBATCH --error=/data/users/nvahdani/flow_project/fastqc_secondupload/error_fastqc_secondupload_%j.e
+#SBATCH --output=/fastqc_secondupload/output_fastqc_secondupload_%j.o
+#SBATCH --error=/fastqc_secondupload/error_fastqc_secondupload_%j.e
 
 module add UHTS/Quality_control/fastqc/0.11.9;
 fastqc -o /data/users/nvahdani/flow_project/fastqc_secondupload/output -f fastq /data/users/nvahdani/flow_project/reads/data_secondupload/*.fastq.gz
