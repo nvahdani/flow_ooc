e04d2aa4adf5fbfd7e89308d6f3ed918a037fbef
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 10:53:12 +0200

    04_fastp.sh

--- preprocessing_meta_analysis/04_fastp.sh
+++ preprocessing_meta_analysis/04_fastp.sh
@@ -4,16 +4,15 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=8000M
 #SBATCH --time=02:00:00
-#SBATCH --mail-user=negar.vahdani@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/nvahdani/flow_project/meta-analysis/fastp_elife/output_fastp_elife_%j.o
-#SBATCH --error=/data/users/nvahdani/flow_project/meta-analysis/fastp_elife/error_fastp_elife_%j.e
+#SBATCH --output=/meta-analysis/fastp_elife/output_fastp_elife_%j.o
+#SBATCH --error=/meta-analysis/fastp_elife/error_fastp_elife_%j.e
 
 module load fastp/0.23.4-GCC-10.3.0;
 
 
-INPUT_DIR=/data/users/nvahdani/flow_project/meta-analysis/data_elife/raw_fastq
-OUTPUT_DIR=/data/users/nvahdani/flow_project/meta-analysis/data_elife/trimmed_fastq
+INPUT_DIR=/meta-analysis/data_elife/raw_fastq
+OUTPUT_DIR=/meta-analysis/data_elife/trimmed_fastq
 
 for file in ${INPUT_DIR}/*.fastq; do
     BASENAME=$(basename "$file" .fastq)
