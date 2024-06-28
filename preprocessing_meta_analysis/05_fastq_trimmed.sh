9b0f9d895da19b99bf866f04d4038cc18a93ef01
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 11:49:24 +0200

    05_fastq_trimmed.sh

--- preprocessing_meta_analysis/05_fastq_trimmed.sh
+++ preprocessing_meta_analysis/05_fastq_trimmed.sh
@@ -10,6 +10,7 @@
 #SBATCH --output=/meta-analysis/fastq_trimmed_elife/error_fastqc_trimmed_elife_%j.e
 #SBATCH --error=/meta-analysis/fastq_trimmed_elife/output_fastqc_trimmed_elife%j.o
 
+# Load the module and run fastqc
 module load FastQC/0.11.9-Java-11;
 fastqc -o /meta-analysis/fastq_trimmed_elife -f fastq /meta-analysis/data_elife/trimmed_fastq/*.fastq
 
