074ef1fdc2f1fa7b565d6e9ff0f2f12cf7015981
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 12:17:34 +0200

    05_samtobam110trimmed.sh

--- preprocessing_flow_data/05_samtobam110trimmed.sh
+++ preprocessing_flow_data/05_samtobam110trimmed.sh
@@ -7,14 +7,16 @@
 #SBATCH --output=/samtobam_110/output_samtobam110trimmed%j.o
 #SBATCH --error=/samtobam_110/error_samtobam110trimmed%j.e
 
+# Define the directories
 OUTDIR="/samtobam_110/output_trimmed"
 SAMDIR="/mapping110/output_trimmed"
 
-
+# Load the module 
+module load UHTS/Analysis/samtools/1.10
+# Create and array and extract the file names
 R1=($(ls -1 /reads/trimmed_data_newversion/*R1*.trimmed.fastq.gz))
 filename=$(basename "${R1[$SLURM_ARRAY_TASK_ID]}"| sed 's/_L.*//')
 base=${filename%.*}
 echo $base
-module load UHTS/Analysis/samtools/1.10
 
 samtools view -hbS $SAMDIR/${base}_mapping.sam  -o $OUTDIR/${base}_mapping.bam
