3091d95a6f6eda8d1ad82e83aa5f2e5b4199ff5a
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 10:51:53 +0200

    02_fastqc.sh

--- preprocessing_meta_analysis/02_fastqc.sh
+++ preprocessing_meta_analysis/02_fastqc.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Job name
 #SBATCH --job-name="QC on raw data"
 #SBATCH --partition=pibu_el8
 #SBATCH --cpus-per-task=2
@@ -8,11 +7,11 @@
 #SBATCH --job-name=fastqc
 #SBATCH --mail-user=negar.vahdani@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/nvahdani/flow_project/meta-analysis/fastq_elife/error_fastqc_elife_%j.e
-#SBATCH --error=/data/users/nvahdani/flow_project/meta-analysis/fastq_elife/output_fastqc_elife%j.o
+#SBATCH --output=/meta-analysis/fastq_elife/error_fastqc_elife_%j.e
+#SBATCH --error=/meta-analysis/fastq_elife/output_fastqc_elife%j.o
 
 module load FastQC/0.11.9-Java-11;
-fastqc -o /data/users/nvahdani/flow_project/meta-analysis/fastq_elife -f fastq /data/users/nvahdani/flow_project/meta-analysis/data_elife/raw_fastq/*.fastq
+fastqc -o /meta-analysis/fastq_elife -f fastq /meta-analysis/data_elife/raw_fastq/*.fastq
 
 
 
