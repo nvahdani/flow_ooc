8f330268425409d3065a5bd88c6d87fc2d1ba71f
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 11:54:24 +0200

    09_bamsort_reverse_stranded.sh

--- preprocessing_meta_analysis/09_bamsort_reverse_stranded.sh
+++ preprocessing_meta_analysis/09_bamsort_reverse_stranded.sh
@@ -6,17 +6,17 @@
 #SBATCH --mem-per-cpu=25G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=sorting
-#SBATCH --mail-user=negar.vahdani@unibe.ch
-#SBATCH --mail-type=end
+
 #SBATCH --error=/meta-analysis/bamsort/error_sorting_%j.e
 #SBATCH --output=/meta-analysis/bamsort/output_sorting_%j.o
 
 
-# define variables
+# Define the directories
 BAMDIR="/meta-analysis/samtobam_reverse_stranded"
 OUTDIR="/meta-analysis/bamsort_reverse_stranded"
 mkdir -p $OUTDIR
 
+# Make an array
 ReadArray=($BAMDIR/*_mapping.bam)
 
 # Select the file corresponding to the SLURM array task ID
@@ -24,7 +24,8 @@ FastqFile=${ReadArray[$SLURM_ARRAY_TASK_ID]}
 
 # Extract the base name without the path and extension
 base=$(basename "$FastqFile" _mapping.bam)
-#load module
+
+#load module and run samtools
 module load SAMtools/1.13-GCC-10.3.0 ;
 
 samtools sort -@ 4 -m 24G -o $OUTDIR/${base}_sorted.bam -T temp_bam $BAMDIR/${base}_mapping.bam
