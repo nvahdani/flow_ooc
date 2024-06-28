c95f96a32644774e8f31246e6d9c4c5e52a1435b
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 12:19:27 +0200

    06_bamsort110trimmed.sh

--- preprocessing_flow_data/06_bamsort110trimmed.sh
+++ preprocessing_flow_data/06_bamsort110trimmed.sh
@@ -6,20 +6,20 @@
 #SBATCH --mem-per-cpu=25G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=sorting
-#SBATCH --mail-user=negar.vahdani@unibe.ch
-#SBATCH --mail-type=end
 #SBATCH --error=/bamsort110/error_sorting_%j.e
 #SBATCH --output=/bamsort110/output_sorting_%j.o
 
 
-# define variables
+# Define variables
 BAMDIR="/samtobam_110/output_trimmed"
 OUTDIR="/bamsort110/output_trimmed"
+
+# Create an array and extract the file names
 R1=($(ls -1 /reads/trimmed_data_newversion/*R1*.trimmed.fastq.gz))
 filename=$(basename "${R1[$SLURM_ARRAY_TASK_ID]}"| sed 's/_L.*//')
 base=${filename%.*}
 
-#load module
+#load module and run
 module load UHTS/Analysis/samtools/1.10
 
 samtools sort -@ 4 -m 24G -o $OUTDIR/${base}_sorted.bam -T temp_bam $BAMDIR/${base}_mapping.bam
