88ac8bc52b8d9902c4dc074ae2a5a29def93ff1d
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 11:02:00 +0200

    07_mapping_unstranded.sh

--- preprocessing_meta_analysis/07_mapping_unstranded.sh
+++ preprocessing_meta_analysis/07_mapping_unstranded.sh
@@ -5,17 +5,16 @@
 #SBATCH --mem-per-cpu=8000MB
 #SBATCH --time=08:00:00
 #SBATCH --job-name=mapping 
-#SBATCH --mail-user=negar.vahdani@unibe.ch
 #SBATCH --mail-type=end
-#SBATCH --error=/data/users/nvahdani/flow_project/meta-analysis/mapping_unstranded/error_mapping_trimmed_%j.e
-#SBATCH --output=/data/users/nvahdani/flow_project/meta-analysis/mapping_unstranded/output_mapping_trimmed_%j.o
+#SBATCH --error=/meta-analysis/mapping_unstranded/error_mapping_trimmed_%j.e
+#SBATCH --output=/meta-analysis/mapping_unstranded/output_mapping_trimmed_%j.o
 
 #load module
 module load HISAT2/2.2.1-gompi-2021a ;
 #reference genome: Hisat2 indexed reference with transcripts
 REF_GENOME="/data/users/nvahdani/flow_project/index110/output/genome_tran"
-OUTPUT_DIR="/data/users/nvahdani/flow_project/meta-analysis/mapping_unstranded"
-INPUT_DIR=/data/users/nvahdani/flow_project/meta-analysis/data_elife/trimmed_fastq
+OUTPUT_DIR="/meta-analysis/mapping_unstranded"
+INPUT_DIR=/meta-analysis/data_elife/trimmed_fastq
 mkdir -p $OUTPUT_DIR
 #creat an array on the raw data
 ReadArray=($INPUT_DIR/*.trimmed.fastq)
