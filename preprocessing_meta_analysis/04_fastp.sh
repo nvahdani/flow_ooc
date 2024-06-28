c3217e6415337d59732743a5893b7426f9c625c1
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 11:48:42 +0200

    04_fastp.sh

--- preprocessing_meta_analysis/04_fastp.sh
+++ preprocessing_meta_analysis/04_fastp.sh
@@ -4,21 +4,20 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=8000M
 #SBATCH --time=02:00:00
-#SBATCH --mail-type=begin,end
 #SBATCH --output=/meta-analysis/fastp_elife/output_fastp_elife_%j.o
 #SBATCH --error=/meta-analysis/fastp_elife/error_fastp_elife_%j.e
 
-module load fastp/0.23.4-GCC-10.3.0;
-
-
+# Define the directories
 INPUT_DIR=/meta-analysis/data_elife/raw_fastq
 OUTPUT_DIR=/meta-analysis/data_elife/trimmed_fastq
 
+# Load the module
+module load fastp/0.23.4-GCC-10.3.0;
+
+# For each file, remove duplicates, overexpressed seq and poly g
 for file in ${INPUT_DIR}/*.fastq; do
     BASENAME=$(basename "$file" .fastq)
     OUTPUT_FILE=${OUTPUT_DIR}/${BASENAME}.trimmed.fastq
     fastp  -i "$file" -o "$OUTPUT_FILE"  --dedup --overrepresentation_analysis  --trim_poly_g
     echo "Adapter removal and trimming completed for $file."
 done
-
-## remove the dedup and the trim-poly g
