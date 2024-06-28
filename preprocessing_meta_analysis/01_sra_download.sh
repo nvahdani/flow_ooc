29ba06e3ae114268b07f91696db41b78c3791292
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 10:51:17 +0200

    01_sra_download.sh

--- preprocessing_meta_analysis/01_sra_download.sh
+++ preprocessing_meta_analysis/01_sra_download.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-# Job name
 #SBATCH --job-name="SRA_download"
-# Runtime and memory
 #SBATCH --time=02:00:00
-# Partition
 #SBATCH --partition=pibu_el8
 #SBATCH --mem-per-cpu=4G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --array=0-6
-#SBATCH --output=/data/users/nvahdani/flow_project/meta-analysis/data_elife/error_loaddata_%j.e
-#SBATCH --error=/data/users/nvahdani/flow_project/meta-analysis/data_elife/output_loaddata_%j.o
+#SBATCH --output=/meta-analysis/data_elife/error_loaddata_%j.e
+#SBATCH --error=/meta-analysis/data_elife/output_loaddata_%j.o
  
 # Load SRA Toolkit module
 module load SRA-Toolkit/3.0.5-gompi-2021a;
  
  
 # Define accession list file
-ACCESSION_LIST=/data/users/nvahdani/flow_project/meta-analysis/data_elife/accession_list.txt
+ACCESSION_LIST=/meta-analysis/data_elife/accession_list.txt
  
  
  
 # Define output directory
-OUTPUT_DIR="/data/users/nvahdani/flow_project/meta-analysis/data_elife/raw_fastq"
+OUTPUT_DIR="/meta-analysis/data_elife/raw_fastq"
  
 # Create output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
