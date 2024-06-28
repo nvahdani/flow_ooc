ee1ad2f2c0c0c0702349a16939642ad7356bf520
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 12:20:00 +0200

    07_featurecount110trimmed.sh

--- preprocessing_flow_data/07_featurecount110trimmed.sh
+++ preprocessing_flow_data/07_featurecount110trimmed.sh
@@ -5,20 +5,18 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=featurecount
-#SBATCH --mail-user=negar.vahdani@unibe.ch
-#SBATCH --mail-type=end
 #SBATCH --error=/featurecount110/error_counttrimmed_%j.e
 #SBATCH --output=/featurecount110/output_counttrimmed_%j.o
 
 
-
+# Define the directories
 BAMDIR="/bamsort110/output_trimmed/*.bam"
 OUTPUT_DIR="/featurecount110/output_trimmed"
 Annotation_DIR="/reference110"
 OUTPUT_FILE="${OUTPUT_DIR}/gene_counts.txt"
 
 
-#load module
+#load module and run
 module load UHTS/Analysis/subread/2.0.1;
 
 featureCounts -p -a $Annotation_DIR/genome.gtf -o $OUTPUT_FILE -s 1 $BAMDIR
