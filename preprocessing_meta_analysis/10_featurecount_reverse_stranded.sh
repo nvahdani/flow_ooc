f9034fd431432c89b20fa24f00c2fef0b914501c
nvahdani <118254100+nvahdani@users.noreply.github.com>
2024-06-28 11:55:09 +0200

    10_featurecount_reverse_stranded.sh

--- preprocessing_meta_analysis/10_featurecount_reverse_stranded.sh
+++ preprocessing_meta_analysis/10_featurecount_reverse_stranded.sh
@@ -5,19 +5,18 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=featurecount
-#SBATCH --mail-type=end
 #SBATCH --error=/meta-analysis/featurecount_reverse_stranded/error_counttrimmed_%j.e
 #SBATCH --output=/meta-analysis/featurecount_reverse_stranded/output_counttrimmed_%j.o
 
 
-
+# Define directories
 BAMDIR="/meta-analysis/bamsort_unstranded/*.bam"
 OUTPUT_DIR="/meta-analysis/featurecount_reverse_stranded"
 Annotation_DIR="/reference110"
 OUTPUT_FILE="${OUTPUT_DIR}/elife_reverse_strand_gene_counts.txt"
 
 
-#load module
+# Load the modules and run featurecount
 module load Subread/2.0.3-GCC-10.3.0  
 
 featureCounts -a $Annotation_DIR/genome.gtf -o $OUTPUT_FILE -s 2 $BAMDIR
